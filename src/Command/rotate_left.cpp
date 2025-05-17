95dec0ad643a821ab8389581ef92e24ee25f1ee7
Dab1d <2006davidferreira@gmail.com>
2025-05-17 17:56:04 +0100

    update nos comentarios

--- src/Command/rotate_left.cpp
+++ src/Command/rotate_left.cpp
@@ -11,29 +11,38 @@ namespace prog {
 
         rotate_left::~rotate_left() {}
 
-        Image *rotate_left::apply(Image *img) {
-            //obter valores da class img
-            int largura = img->width();
-            int altura = img->height();
+            Image *rotate_left::apply(Image *img) {
+                //obter valores da class img
+                int largura = img->width();
+                int altura = img->height();
 
-            //img com dimensões trocadas
-            Image *newImg = new Image(altura ,largura);
+                //img com dimensões trocadas
+                Image *newImg = new Image(altura ,largura);
 
-            //loop para rodar para a esquerda através da matriz de rotação [cos(90)  -sin(90)] [x]
-            //                                                             [sin(90)   cos(90)] [y]
-            //x' = -y e y' = x, e ajustando ao refencial da imagem(canto superior esquerdo)
-            //x'= y e y =
 
-            for (int y = 0;y < altura;y++) {
-                for (int x = 0;x < largura;x++) {
-                    Color base = img->at(x, y);
-                    newImg->at(y,largura-x-1) = base;
+                // loop para rodar para a esquerda através da matriz de rotação:
+                //
+                //    M(90º) = [cos(90°)  -sin(90°)]   [0  -1]
+                //             [sin(90°)   cos(90°)] = [1   0]
+                //
+                //  M(90º)[x',y'] ==> [-y,x]
+                //
+                // e considerando que o eixo oY aponta para baixo y' = -x,
+                //e o sistema de coordenados da imagem que começa no top left corner temos que
+                //
+                //          x'= y e y' = largura -1 -x
+
+
+                for (int y = 0;y < altura;y++) {
+                    for (int x = 0;x < largura;x++) {
+                        Color base = img->at(x, y);
+                        newImg->at(y,largura-x-1) = base;
+                    }
                 }
-            }
 
-            delete img;
-            return newImg;
-        }
+                delete img;
+                return newImg;
+            }
 
         std::string rotate_left::toString() const {
             std::ostringstream ss;
--- src/Command/rotate_right.cpp
+++ src/Command/rotate_right.cpp
@@ -19,11 +19,24 @@ namespace prog {
             //img com dimensões trocadas
             Image *newImg = new Image(altura ,largura);
 
-            //loop para inverter cada pixel da img
+
+            // loop para rodar para a direita através da matriz de rotação:
+            //
+            //    M(90º) = [cos(270°)  -sin(270°)]   [0  1]
+            //             [sin(270°)   cos(270°)] = [-1 0]
+            //
+            //  M(90º)[x',y'] ==> [y,-x]
+            //
+            // e considerando que o eixo oY aponta para baixo y' = -(-x),
+            //e o sistema de coordenados da imagem que começa no top left corner temos que
+            //
+            //          x'= altura -1 -y e y' = x
+
             for (int y = 0;y < altura;y++) {
                 for (int x = 0;x < largura;x++) {
                     Color base = img->at(x, y);
-                    newImg->at(altura -y-1,x) = base;
+
+                    newImg->at(altura-1 -y ,x) = base;
                 }
             }
 
